dabe385b563bc1144d7a4ce5811320261ebe3ebe
Atticus <atticus.lin@gmail.com>
2017-12-02 14:52:17 -0500

    Fix date checker

--- jetblue.py
+++ jetblue.py
@@ -33,29 +33,23 @@ def is_in_date_range(start, end, target):
 	uses_slashes = False
 	uses_dashes = False
 
-
-	if start.find('/') != -1:
+	if target.find('/') != -1:
 		uses_slashes = True
-	elif start.find('-') != -1:
+	elif target.find('-') != -1:
 		uses_dashes = True
 
 	if uses_dashes:
-		start = start.split('-')
-		end = end.split('-')
 		target = target.split('-')
-
-		start_date = datetime.date(int(start[0]), int(start[1]), int(start[2][:2]))
-		end_date = datetime.date(int(end[0]), int(end[1]), int(end[2][:2]))
 		target_date = datetime.date(int(target[0]), int(target[1]), int(target[2][:2]))
-
 	elif uses_slashes: 
-		start = start.split('/')
-		end = end.split('/')
 		target = target.split('/')
-
-		start_date = datetime.date(int(start[2][:4]), int(start[0]), int(start[1]))
-		end_date = datetime.date(int(end[2][:4]), int(end[0]), int(end[1]))
 		target_date = datetime.date(int(target[2][:4]), int(target[0]), int(target[1]))
+
+	start = start.split("/")
+	end = end.split("/")
+
+	start_date = datetime.date(int(start[2]), int(start[0]), int(start[1]))
+	end_date = datetime.date(int(end[2]), int(end[0]), int(end[1]))
 
 	print('target: ', target_date)
 	print('start: ', start_date)
@@ -65,20 +59,21 @@ def is_in_date_range(start, end, target):
 
 #Processes information
 #Assumes start_date and end_date in form of 'mm/dd/yyyy'
-def process(deals, low_fares, start_date, end_date, depart_code, dest_code):
-	left_date = start_date.split('/')
-	right_date = end_date.split('/')
-	left_date = datetime.date(int(left_date[2]), int(left_date[0]), int(left_date[1]))
-	right_date = datetime.date(int(right_date[2]), int(right_date[0]), int(right_date[1]))
+def process(deals, low_fares, left_date, right_date, depart_code, dest_code):
+	# left_date = start_date.split('/')
+	# right_date = end_date.split('/')
+	# left_date = datetime.date(int(left_date[2]), int(left_date[0]), int(left_date[1]))
+	# right_date = datetime.date(int(right_date[2]), int(right_date[0]), int(right_date[1]))
 
 	deals_in_range = []
 	lowest_fares_in_range = []
 	deal_list = deals[(depart_code,dest_code)]
 	low_list = low_fares[(depart_code, dest_code)]
 	for flight in deal_list:
-		if is_in_date_range(left_date, right_date, flight[2]):
+		print(flight[3])
+		if is_in_date_range(left_date, right_date, flight[3]):
 			deals_in_range.append(flight)
-
+	print(low_list)
 	for flight in low_list:
 		if is_in_date_range(left_date, right_date, flight[2]):
 			lowest_fares_in_range.append(flight)
@@ -91,8 +86,7 @@ def cheapest_flight(deals_list):
 	#minimum = sys.maxsize()
 	return min(deals_list, key = lambda flight: flight[4]) 
 
-
 if(__name__ == "__main__"):
 	print("Hello World")
-	print(is_in_date_range('12/1/1998 123', '12/12/2017 2123', '1/1/2000 123'))
-	print(is_in_date_range('12/1/2010 123', '12/12/2017 2123', '1/1/2000 123'))
+	# print(is_in_date_range('12/1/1998 123', '12/12/2017 2123', '1/1/2000 123'))
+	# print(is_in_date_range('12/1/2010 123', '12/12/2017 2123', '1/1/2000 123'))
