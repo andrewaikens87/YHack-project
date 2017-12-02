40e3549dee6b12a59161219e123a10c20e2701c1
andrewaikens87 <andrewaikens87@gmail.com>
2017-12-02 15:35:33 -0500

    fix quote char

--- jet_blue_flask.py
+++ jet_blue_flask.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 	first = True
 
 	with open('Deals.csv', newline='') as deals:
-		reader_deals = csv.reader(deals, delimiter=',', quotechar="|")
+		reader_deals = csv.reader(deals, delimiter=',', quotechar="\"")
 		for row in list(reader_deals):
 			if(first):
 				first = False
